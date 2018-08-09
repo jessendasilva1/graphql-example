ac552aca230ab831810d332732683219b5e65614
jessendasilva1 <jessen>
2018-08-09 17:11:53 -0400

    testing

--- index.js
+++ index.js
@@ -1,65 +1,12 @@
+import { ApolloEngine } from 'apollo-engine';
+const engine = new ApolloEngine();
 
-const { ApolloServer, gql } = require('apollo-server');
+// Set a default value of 3000 if we don't pass an env var
+const PORT = process.env.PORT || 3000;
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
-const books = [
-  {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
-];
-
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
-const typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
-
-  # This "Book" type can be used in other type declarations.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
-  type Query {
-    books: [Book]
-  }
-`;
-
-// Resolvers define the technique for fetching the types in the
-// schema.  We'll retrieve books from the "books" array above.
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
-
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers });
-
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-  });
-  
-/*
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) =>{
-    res.send({'hi': 'there'});
-})
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
-*/
+engine.listen({
+  port: PORT,
+  expressApp: app
+}, () => {
+  console.log(`Server running on port ${PORT}!`);
+});
