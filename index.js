a01f0fdc77af0e77576eb4be54a63d06a6d503dc
jessendasilva1 <jessen>
2018-08-09 17:15:26 -0400

    testing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { ApolloEngine } from 'apollo-engine';
+const ApolloEngine =require('apollo-engine');
 const engine = new ApolloEngine();
 
 // Set a default value of 3000 if we don't pass an env var
